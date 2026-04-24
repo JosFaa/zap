aea14ca3e201b10a48797a55c2666d834f3a7c8a
Joshua <joshua.faak@tuhh.de>
2026-04-24 13:23:50 +0200

    add handling trailing whitespace in number parser

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <ctype.h>
 
 int main () {
     char buffer[256];
@@ -12,7 +13,12 @@ int main () {
         num = strtol(buffer, &endptr, 10);
         if (buffer == endptr) {
             printf("no digits were found.\n");
-        } else if (*endptr != '\n') {
+            continue;
+        } 
+        while (isspace((unsigned char)*endptr)) {
+            endptr++;
+        }
+        if (*endptr != '\0') {
             printf("Invalid character: %c\n", *endptr);
         } else if (errno == ERANGE) {
             printf("Number is out of range.\n");
